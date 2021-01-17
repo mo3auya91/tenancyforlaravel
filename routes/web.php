a45d93a00fe58e98a78faa8a6689cbad39245419
moauya <mo3awia91@yandex.com>
2021-01-17 19:54:37 +0200

    remove .idea from git

--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,16 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-//    $tenant1 = Tenant::create(['id' => 'foo']);
-//    $tenant1->domains()->create(['domain' => 'foo.'.env('APP_BASE_URL')]);
-//
-//    $tenant2 = Tenant::create(['id' => 'bar']);
-//    $tenant2->domains()->create(['domain' => 'bar.'.env('APP_BASE_URL')]);
-//
-//    Tenant::all()->runForEach(function () {
-//        User::factory()->create();
-//    });
+    $tenant1 = Tenant::create(['id' => 'foo']);
+    $tenant1->domains()->create(['domain' => 'foo.' . env('APP_BASE_URL')]);
+
+    $tenant2 = Tenant::create(['id' => 'bar']);
+    $tenant2->domains()->create(['domain' => 'bar.' . env('APP_BASE_URL')]);
+
+    Tenant::all()->runForEach(function () {
+        User::factory()->create();
+    });
 
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
